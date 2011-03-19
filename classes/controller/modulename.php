0ba0834189a5481461b14f0a52e9127aa2c036ea
Sebastian Castro <sebicas@gmail.com>
2011-03-19 16:48:31 -0300

    Config Values Fixes...

--- classes/controller/modulename.php
+++ classes/controller/modulename.php
@@ -16,8 +16,12 @@ class Controller_Modulename extends Controller {
 
     public function action_index()
     {
+
+        // Load module configuration
+        $some_config_value = Kohana::config('modulename.some_config_value');
+
         // Say hi!
-        $this->response->body('hello, module!');
+        $this->response->body('Hey.. Hello Modulename! ' . $some_config_value);
     }
 
 } // End Welcome
--- config/modulename.php
+++ config/modulename.php
@@ -6,11 +6,6 @@ return array(
 	 * This a config value I like to pass to my module
 	 * Note: http://www.mywebsite.com/some-extra-info-on-the-value
 	 */
-	'some-config-value' => 'Hey!! Here the value we passed from the config file. :)',
-
-	/**
-	 * This a config bolean value I like to Pass to my module
-	 */
-	'show-config-value' => FALSE,
+	'some_config_value' => 'Here is the value we passed from the config file. :)',
 
 );
