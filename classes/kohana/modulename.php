b25bc1b1eb0e93f74c7d303a622d09f603cc9307
Sebastian Castro <sebicas@gmail.com>
2011-03-19 14:34:34 -0300

    Add some code to module classes

---
+++ classes/kohana/modulename.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+/**
+ * Modulename — My own custom module.
+ *
+ * @package    Modulename
+ * @category   Base
+ * @author     Myself Team
+ * @copyright  (c) 2012 Myself Team
+ * @license    http://kohanaphp.com/license.html
+ */
+abstract class Kohana_Modulename {}
--- classes/modulename.php
+++ classes/modulename.php
@@ -1,7 +1,3 @@
-<?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+<?php defined('SYSPATH') or die('No direct script access.');
 
-?>
+class Modulename extends Kohana_Modulename {}
