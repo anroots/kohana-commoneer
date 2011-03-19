9163b1f0ca552841f71e87f1909a58ac02010971
Sebastian Castro <sebicas@gmail.com>
2011-03-19 16:05:43 -0300

    Some updates

--- classes/model/modulemodelname.php
+++
@@ -1,7 +0,0 @@
-<?php
-/* 
- * You should have all the Module's Models inside the Module Folder
- * so the module can be functional just by copying the module folder.
- */
-
-?>
---
+++ classes/model/modulename.php
@@ -0,0 +1,15 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+/*
+ * You should have all the module's models inside the module folder
+ * so the module can be functional just by copying the module folder.
+ *
+ * It's recomended to name you Module's Models as Model_Modulename_<Model>
+ * to avoid any conflics with any application model
+ */
+
+class Model_Modulename extends Model {
+
+	// This class can be replaced or extended
+
+} // End User Model
