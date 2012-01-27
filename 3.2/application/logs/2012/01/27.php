287a0a5a77d83a199e8c26015838b9869b70c0e3
Ando Roots <andoroots@gmail.com>
2012-01-27 12:19:15 +0200

    Bootstrap 2pre, chosen

---
+++ 3.2/application/logs/2012/01/27.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2012-01-27 11:32:38 --- ERROR: ErrorException [ 1 ]: Call to a member function directory() on a non-object ~ MODPATH/commoneer-1.4/classes/commoneer/assets.php [ 258 ]
+2012-01-27 11:32:38 --- STRACE: ErrorException [ 1 ]: Call to a member function directory() on a non-object ~ MODPATH/commoneer-1.4/classes/commoneer/assets.php [ 258 ]
+--
+#0 [internal function]: Kohana_Core::shutdown_handler()
+#1 {main}
