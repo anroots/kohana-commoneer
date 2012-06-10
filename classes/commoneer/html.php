e3f6df517c945127aa829d9834bf03e95eceec41
Ando Roots <ando@roots.ee>
2012-06-10 06:07:02 +0300

    HTML::breadcrumbs can now accept undefined number of args

--- classes/commoneer/html.php
+++ classes/commoneer/html.php
@@ -4,15 +4,13 @@
  *
  * @package Commoneer
  * @category Helpers
- * @author Ando Roots 2012
+ * @author Ando Roots <anroots@itcollege.ee>
  * @since 1.4
  */
-class Commoneer_HTML extends Kohana_HTML
-{
+class Commoneer_HTML extends Kohana_HTML {
 
 	/**
 	 * Holds the current array of breadcrumbs
-	 *
 	 * Filled via calls to HTML::breadcrumbs
 	 *
 	 * @var array Array of breadcrumbs
@@ -23,42 +21,49 @@ class Commoneer_HTML extends Kohana_HTML
 
 	/**
 	 * Breadcrumb builder. Use to build the contents of the breadcrumb bar.
-	 *
 	 * Call with no parameters to output current breadcrumbs.
 	 * Use $link and $text to add a new breadcrumb to the stack
 	 * Call this method in your controller(s) to build the breadcrumb stack
 	 * ...and echo the result in your template view
 	 *
 	 * @example HTML::breadcrumbs('admin', 'Main page'); echo HTML::breadcrumbs();
+	 * @example HTML::breadcrumbs(array('admin' => 'Main page'));
+	 * @example HTML::breadcrumbs(array('admin' => 'Main page'), array('admin/users' => 'Users'));
 	 * @since 1.4
 	 * @static
-	 * @param null|string|array $link The link of the breadcrumb. Base URL is appended automatically.
-	 * @param null|string $text The text of the breadcrumb. Automatically translated
-	 * @return bool|null|View True when adding breadcrumbs, HTML string of all added breadcrumbs when called with no params.
+	 * @param null|string|array $link The link(s) of the breadcrumb. Base URL is appended automatically.
+	 * @param null|string $text The text of the breadcrumb. Automatically translated. Can be empty if $link is an array.
+	 * @return string|null|bool Rendered HTML when called with no params or TRUE
 	 */
 	public static function breadcrumbs($link = NULL, $text = NULL)
 	{
-		// Add a new breadcrumb?
-		if (($link !== NULL && $text !== NULL) || is_array($link)) {
+		if (empty($link) && empty($text)) {
+			// Return HTML for all added breadcrumbs
+			return View::factory('templates/breadcrumbs', array('breadcrumbs' => self::$_breadcrumbs));
+		}
 
-			if (!is_array($link)) {
-				$link = array($link => $text);
+		// Add a new breadcrumb?
+		if ($link !== NULL) {
+
+			// Add only one breadcrumb
+			if (! is_array($link)) {
+				self::$_breadcrumbs[] = array('link' => $link,
+				                              'text' => __((string) $text)
+				);
+				return TRUE;
 			}
+
+			// The function can also be called with undefined number of array arguments
+			$args = func_get_args();
 
 			// Add all input breadcrumbs (1...n)
-			foreach ($link as $url => $text) {
-				HTML::$_breadcrumbs[] = array('link' => $url, 'text' => __($text));
+			foreach ($args as $crumb) {
+				self::$_breadcrumbs[] = array('link' => key($crumb),
+				                              'text' => __($crumb[key($crumb)])
+				);
 			}
 
-			return TRUE;
 		}
-
-		// Any breadcrumbs to display?
-		if (empty(HTML::$_breadcrumbs)) {
-			return NULL;
-		}
-
-		// Return HTML for all added breadcrumbs
-		return View::factory('templates/breadcrumbs', array('breadcrumbs' => HTML::$_breadcrumbs));
+		return TRUE;
 	}
 }
