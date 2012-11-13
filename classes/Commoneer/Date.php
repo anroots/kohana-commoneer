2118c049e7790b0b83c94d0f61e38a62693d0722
Ando Roots <ando@sqroot.eu>
2012-11-13 16:13:10 +0200

    Move Date::date_smaller_than to Valid

--- classes/Commoneer/Date.php
+++ classes/Commoneer/Date.php
@@ -123,42 +123,6 @@ class Commoneer_Date extends Kohana_Date {
 	}
 
 
-	/**
-	 * Check if the start date is smaller than the end date
-	 * Useful in validation rules
-	 *
-	 * @since 1.0
-	 * @param string|int|DateTime $start Any date string or timestamp value
-	 * @param string|int|DateTime $end Any date string or timestamp value
-	 * @return bool TRUE if $start is strictly smaller than $end
-	 */
-	public static function date_smaller_than($start, $end)
-	{
-		if ($start instanceof DateTime) {
-			$start = $start->getTimestamp();
-		}
-
-		if ($end instanceof DateTime) {
-			$end = $end->getTimestamp();
-		}
-
-		if (
-			(! is_numeric($start) && ! is_string($start))
-			|| (! is_numeric($end) && ! is_string($end))
-		) {
-			return FALSE;
-		}
-
-		// Converts input to timestamps if needed
-		if (! $ts1 = strtotime($start)) {
-			$ts1 = $start;
-		}
-		if (! $ts2 = strtotime($end)) {
-			$ts2 = $end;
-		}
-
-		return $ts1 < $ts2;
-	}
 
 
 	/**
